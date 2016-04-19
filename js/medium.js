4173ff023a6407424afcd25e6080a5920496e8c9
Meng-Ying LU <zxc22884@gmail.com>
2016-04-19 23:16:34 +0800

    Added files via upload

---
+++ js/medium.js
@@ -0,0 +1,34 @@
+var main = function() {
+  $('#o_res1').click(function(){
+    $('.menu3').animate({
+      left : '44.5%'
+    },2000,function(){
+      $('#iframe2').load("./html/child_order_html/res1.html");
+      $('menu3').animate({
+        left : '66.5%'
+      },2000);
+    });
+  });
+  $('#o_res2').click(function(){
+    $('.menu3').animate({
+      left : '44.5%'
+    },2000,function(){
+      $('#iframe2').load("./html/child_order_html/res2.html");
+      $('menu3').animate({
+        left : '66.5%'
+      },2000);
+    });
+  });
+  $('#o_res3').click(function(){
+    $('.menu3').animate({
+      left : '44.5%'
+    },2000,function(){
+      $('#iframe2').load("./html/child_order_html/res3.html");
+      $('menu3').animate({
+        left : '66.5%'
+      },2000);
+    });
+  });
+}
+
+$(document).ready(main);

4e0bd10e6ac7d259faf029b5ff68b6e7192013ec
Meng-Ying LU <zxc22884@gmail.com>
2016-04-19 23:30:27 +0800

    Added files via upload

--- js/medium.js
+++ js/medium.js
@@ -4,7 +4,7 @@ var main = function() {
       left : '44.5%'
     },2000,function(){
       $('#iframe2').load("./html/child_order_html/res1.html");
-      $('menu3').animate({
+      $('.menu3').animate({
         left : '66.5%'
       },2000);
     });
@@ -14,7 +14,7 @@ var main = function() {
       left : '44.5%'
     },2000,function(){
       $('#iframe2').load("./html/child_order_html/res2.html");
-      $('menu3').animate({
+      $('.menu3').animate({
         left : '66.5%'
       },2000);
     });
@@ -24,7 +24,7 @@ var main = function() {
       left : '44.5%'
     },2000,function(){
       $('#iframe2').load("./html/child_order_html/res3.html");
-      $('menu3').animate({
+      $('.menu3').animate({
         left : '66.5%'
       },2000);
     });

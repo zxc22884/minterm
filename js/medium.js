f90146dd0e9f2000da0a36eb22f581bf1dba5b06
Meng-Ying LU <zxc22884@gmail.com>
2016-04-19 23:36:35 +0800

    Added files via upload

--- js/medium.js
+++ js/medium.js
@@ -1,31 +1,31 @@
 var main = function() {
   $('#o_res1').click(function(){
     $('.menu3').animate({
-      left : '44.5%'
+      left : '44%'
     },2000,function(){
       $('#iframe2').load("./html/child_order_html/res1.html");
       $('.menu3').animate({
-        left : '66.5%'
+        left : '66%'
       },2000);
     });
   });
   $('#o_res2').click(function(){
     $('.menu3').animate({
-      left : '44.5%'
+      left : '44%'
     },2000,function(){
       $('#iframe2').load("./html/child_order_html/res2.html");
       $('.menu3').animate({
-        left : '66.5%'
+        left : '66%'
       },2000);
     });
   });
   $('#o_res3').click(function(){
     $('.menu3').animate({
-      left : '44.5%'
+      left : '44%'
     },2000,function(){
       $('#iframe2').load("./html/child_order_html/res3.html");
       $('.menu3').animate({
-        left : '66.5%'
+        left : '66%'
       },2000);
     });
   });
